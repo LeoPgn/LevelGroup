114084c03351c83f09604ebc459d9f4b5c0c6f78
Leonardo Paganini <leosonfire01@outlook.com>
2023-10-23 22:24:59 -0300

    Upload_Enterprise_Entregável

---
+++ sprint01-Enterprise/sprint01-Enterprise/AeroParts/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("oracle");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
